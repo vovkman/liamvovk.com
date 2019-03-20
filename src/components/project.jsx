5848aab6691d979010e5a001c5ba3dfc52f17d43
Liam Vovk <liam.vovk@gmail.com>
2019-03-19 21:24:29 -0400

    Update contact links

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -1,10 +1,10 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const Project = ({ match }) => {
   const project = projects[match.params.project];
-  const nextClass = project.next !== "#" ? "light-black" : "light-grey";
-  const prevClass = project.prev !== "#" ? "light-black" : "light-grey";
+  const nextClass = project.next !== '#' ? 'light-black' : 'light-grey';
+  const prevClass = project.prev !== '#' ? 'light-black' : 'light-grey';
   return (
     <div className="flex-column">
       <div className="flex-row flex-column-reverse-nl pt7 pt5-nxl">
@@ -44,6 +44,7 @@ const renderProjectImages = project => (
           }`}
           src={src}
           key={key}
+          alt={project.img_alt}
         />
         {project.img_description && (
           <div className="paragraph-3 pb3 w100 text-center">
@@ -57,39 +58,43 @@ const renderProjectImages = project => (
 
 export const projects = {
   gighub: {
-    title: "GigHub",
+    title: 'GigHub',
     description:
-      "Implemented an iOS and Android application to help connect students with experiential learning opportunies. The application was developed with React Native, NodeJS, and MongoDB, hosted on AWS EC2 instances.",
-    img_src: ["/gighub-landing.png", "/gighub-profile.png", "/gighub-gigs.png"],
-    img_css: "gighub-imgs",
-    next: "/work/ownr/",
-    prev: "#"
+      'Implemented an iOS and Android application to help connect students with experiential learning opportunies. The application was developed with React Native, NodeJS, and MongoDB, hosted on AWS EC2 instances.',
+    img_src: ['/gighub-landing.png', '/gighub-profile.png', '/gighub-gigs.png'],
+    img_css: 'gighub-imgs',
+    img_alt: 'Gighub',
+    next: '/work/ownr/',
+    prev: '#'
   },
   ownr: {
-    title: "OWNR",
+    title: 'OWNR',
     description:
-      "As a software developer for RBC Ventures I work on the OWNR team. Here I develop full stack applications using NodeJS, React with Redux, and TypeScript, using Observables (rxjs) extensively to handle async operations and sequencing of actions. Outside of pure development my responsibilities includ developing a deep understanding of website build and delivery process in order to optimize user experience on slow networks, and the technical implementation of Google Analytics to conform with RBC security standards for RBC Ventures apps hosted in the cloud. My role with GA has given me the opportunity to work extensively with product, marketing, design, and internal RBC teams, giving me a well rounded view of what it takes to convert users into paying customers, as well as how to manage integrations between RBC Ventures cloud and internal RBC.",
-    img_src: ["/ownr-1.png", "/ownr-2.png", "/ownr-3.png"],
-    img_css: "ownr-imgs",
-    prev: "/work/gighub/",
-    next: "/work/automated_core_logging/"
+      'As a software developer for RBC Ventures I work on the OWNR team. Here I develop full stack applications using NodeJS, React with Redux, and TypeScript, using Observables (rxjs) extensively to handle async operations and sequencing of actions. Outside of pure development my responsibilities includ developing a deep understanding of website build and delivery process in order to optimize user experience on slow networks, and the technical implementation of Google Analytics to conform with RBC security standards for RBC Ventures apps hosted in the cloud. My role with GA has given me the opportunity to work extensively with product, marketing, design, and internal RBC teams, giving me a well rounded view of what it takes to convert users into paying customers, as well as how to manage integrations between RBC Ventures cloud and internal RBC.',
+    img_src: ['/ownr-1.png', '/ownr-2.png', '/ownr-3.png'],
+    img_css: 'ownr-imgs',
+    img_alt: 'OWNR',
+    prev: '/work/gighub/',
+    next: '/work/automated_core_logging/'
   },
   automated_core_logging: {
-    title: "Computer Vision",
+    title: 'Computer Vision',
     description:
-      "Designed a computer vision algorithm for extracting critical angles from rock core using a combination of ELSD, k-means, and ODR. The final product reduced a 30 minute process to 30 seconds.",
-    img_src: ["/core-summary.png"],
-    img_css: "core-imgs",
-    img_description: ["Mapping core breaks onto a 3D plane"],
-    prev: "/work/gighub/",
-    next: "/work/personal_site/"
+      'Designed a computer vision algorithm for extracting critical angles from rock core using a combination of ELSD, k-means, and ODR. The final product reduced a 30 minute process to 30 seconds.',
+    img_src: ['/core-summary.png'],
+    img_css: 'core-imgs',
+    img_alt: 'Thesis',
+    img_description: ['Mapping core breaks onto a 3D plane'],
+    prev: '/work/gighub/',
+    next: '/work/personal_site/'
   },
   personal_site: {
-    title: "liamvovk.com",
-    description: "Developed by me for fun :)",
-    img_src: ["/taranaki-full.jpg"],
-    img_css: "personal-imgs",
-    prev: "/work/ownr/",
-    next: "#"
+    title: 'liamvovk.com',
+    description: 'Developed by me for fun :)',
+    img_src: ['/taranaki-full.jpg'],
+    img_css: 'personal-imgs',
+    img_alt: 'Personal Website',
+    prev: '/work/ownr/',
+    next: '#'
   }
 };
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 export const Contact = () => (
   <div className="about flex-row flex-column-nxl pt5">
@@ -17,13 +17,36 @@ export const Contact = () => (
 const renderSocialImgs = () => (
   <div className="flex-row justify-between items-center">
     {contactImgInfo.map((img, key) => (
-      <img className="mini-img" src={img.img_src} key={key} />
+      <a
+        href={img.img_href}
+        className="flex-column items-center contact-link"
+        target="_blank"
+        rel="noopener noreferrer"
+        key={key}
+      >
+        <img className="contact-img" src={img.img_src} alt={img.img_alt} />
+      </a>
     ))}
   </div>
 );
 
 const contactImgInfo = [
-  { img_src: "/github.svg", img_css: "github" },
-  { img_src: "/google-plus.svg", img_css: "google-plus" },
-  { img_src: "/linkedin.svg", img_css: "linkedin" }
+  {
+    img_src: '/github.svg',
+    img_css: 'github',
+    img_href: 'https://github.com/vovkman/',
+    img_alt: 'github'
+  },
+  {
+    img_src: '/google-plus.svg',
+    img_css: 'google-plus',
+    img_href: 'mailto:liam.vovk@gmail.com',
+    img_alt: 'email'
+  },
+  {
+    img_src: '/linkedin.svg',
+    img_css: 'linkedin',
+    img_href: 'https://www.linkedin.com/in/liamvovk/',
+    img_alt: 'LinkedIn'
+  }
 ];
