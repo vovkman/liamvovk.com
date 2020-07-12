86cc13406860b4938cbdb2932b914de5db4e6667
Liam Vovk <vovkliam@amazon.com>
2020-07-12 11:55:32 -0700

    Updates

---
+++ precache-manifest.968c55e0c54be7764e88d272eb18bcd2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b72f5b5b65196bb50d3",
+    "url": "/static/css/main.15122b06.chunk.css"
+  },
+  {
+    "revision": "6b72f5b5b65196bb50d3",
+    "url": "/static/js/main.6b72f5b5.chunk.js"
+  },
+  {
+    "revision": "3e5bd12767eda7838317",
+    "url": "/static/js/1.3e5bd127.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a26f33075a5c3b482aea93900d75211c",
+    "url": "/index.html"
+  }
+];
